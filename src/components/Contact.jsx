d5f9cda392ef01d951740be4e59408c9a39a14a2
kelly-Moon <kellymoon220@gmail.com>
2026-03-29 19:33:15 +0900

    연락처 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+const contactText = [
+  {
+    link: "https://kr.pinterest.com/",
+    title: "Continue the Journey",
+  },
+  {
+    link: "https://ko.react.dev/",
+    title: "Explore More Mountains",
+  },
+];
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact_inner">
+        <h2 className="contact_title">Contact</h2>
+        <div className="contact_lines top">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <div className="contact_text">
+          <div className="text">
+            {contactText.map((contact, key) => (
+              <div key={key}>
+                <a href={contact.link} target="_blank">
+                  {contact.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact_lines bottom">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
