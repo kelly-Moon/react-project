25374c8703ec0bc2df434d2b6e24785a2cb16da5
kelly-Moon <kellymoon220@gmail.com>
2026-03-29 15:02:58 +0900

    인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
+import about from "../assets/img/about.jpg";
+
+const introText = {
+  title: "Into the Mountains",
+  decs: ["Step by Step", "Toward the Top"],
+};
+
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <section id="intro">
+      <div className="intro_inner">
+        <div className="intro_title">{introText.title}</div>
+        <div className="intro_lines">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="intro_text">
+          <div className="text">
+            <div>{introText.decs[0]}</div>
+            <div>{introText.decs[1]}</div>
+          </div>
+          <div className="img">
+            <img src={about} alt="" />
+          </div>
+        </div>
+        <div className="intro_lines bottom">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
